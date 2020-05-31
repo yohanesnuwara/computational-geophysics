d4025ababdabf4ed514ce16998448e9503c85b3a
yohanesnuwara <51282928+yohanesnuwara@users.noreply.github.com>
2020-05-31 23:13:23 +0700

    Update seis_util.py

--- seismic/seis_util.py
+++ seismic/seis_util.py
@@ -107,6 +107,141 @@ def slice_cube(cube, type='il',
       plt.axis('equal')
       plt.show()  
 
+def frequency_spectrum(data, type='il', inline_array=None, xline_array=None, timeslice_array=None, sample_rate=0.004):
+
+  """
+  Compute the frequency spectrum of the whole cube, inline, or crossline
+  cube
+  (Copyright, Y. Nuwara, ign.nuwara97@gmail.com)
+
+  Input:
+
+  data: if you choose 'whole', data must be in 3D numpy array, if you choose
+        either 'il' or 'xl', data must be in 2D numpy array (you should make
+        the slice first using 'slicing' function)
+  type: 'whole' for the whole cube, 'il' for inline section, 'xl' for
+        crossline section 
+
+  inline_array (inline), xline_array (xline), timeslice_array (time) 
+  sample_rate: sampling rate of seismic data (in second) 
+
+  Output:
+
+  freq_seis: frequency
+  spec_seis: seismic amplitude
+  
+  """
+
+  import numpy as np
+  import matplotlib.pyplot as plt
+
+  if type == 'il':
+
+    # transpose the slice 
+    transp_slice = np.transpose(data)
+
+    # take the average of each individual line traces
+
+    # time
+    min_time = 0
+    max_time = len(timeslice_array)
+
+    # crosslines
+    xmin = 0
+    xmax = len(xline_array)
+
+    trace = np.mean(transp_slice[min_time:max_time, xmin:xmax], axis=1)
+
+    Fs_seis = 1 / sample_rate  # Seconds.
+    n_seis = len(trace)
+    k_seis = np.arange(n_seis)
+    T_seis = n_seis / Fs_seis
+    freq_seis = k_seis / T_seis
+    freq_seis = freq_seis[range(n_seis//2)]  # One side frequency range.
+
+    spec_seis = np.fft.fft(trace) / n_seis  # FFT computing and normalization.
+    spec_seis = spec_seis[range(n_seis//2)]
+
+    # This is to smooth the spectrum over a window of 10.
+    roll_win = np.ones(10) / 10
+    spec_seis = np.convolve(spec_seis, roll_win, mode='same')
+
+  if type == 'xl':
+
+    # transpose the slice 
+    transp_slice = np.transpose(data)
+
+    # take the average of each individual line traces
+
+    # time
+    min_time = 0
+    max_time = len(timeslice_array)
+
+    # crosslines
+    xmin = 0
+    xmax = len(inline_array)
+
+    trace = np.mean(transp_slice[min_time:max_time, xmin:xmax], axis=1)
+
+    Fs_seis = 1 / sample_rate  # Seconds.
+    n_seis = len(trace)
+    k_seis = np.arange(n_seis)
+    T_seis = n_seis / Fs_seis
+    freq_seis = k_seis / T_seis
+    freq_seis = freq_seis[range(n_seis//2)]  # One side frequency range.
+
+    spec_seis = np.fft.fft(trace) / n_seis  # FFT computing and normalization.
+    spec_seis = spec_seis[range(n_seis//2)]
+
+    # This is to smooth the spectrum over a window of 10.
+    roll_win = np.ones(10) / 10
+    spec_seis = np.convolve(spec_seis, roll_win, mode='same')
+  
+  if type == 'whole':
+
+    # transpose the cube
+    transp_cube = np.transpose(data)
+
+    # time
+    min_time = 0
+    max_time = len(timeslice_array)
+
+    # crosslines
+    xmin = 0
+    xmax = len(xline_array)
+
+    # inlines
+    ymin = 0
+    ymax = len(inline_array)
+
+    mean_xl_traces = [] # mean of crossline traces of each inline section
+
+    for i in range(ymax):
+      mean_xl = np.mean(transp_cube[min_time:max_time, xmin:xmax, i], axis=1)
+      mean_xl_traces.append(mean_xl)
+
+    transp_xl = np.transpose(mean_xl_traces)
+
+    # take average of each individual mean values of xl in the inline section
+
+    trace = np.mean(transp_xl[min_time:max_time, ymin:ymax], axis=1)
+
+    Fs_seis = 1 / sample_rate  # Seconds.
+    n_seis = len(trace)
+    k_seis = np.arange(n_seis)
+    T_seis = n_seis / Fs_seis
+    freq_seis = k_seis / T_seis
+    freq_seis = freq_seis[range(n_seis//2)]  # One side frequency range.
+
+    spec_seis = np.fft.fft(trace) / n_seis  # FFT computing and normalization.
+    spec_seis = spec_seis[range(n_seis//2)]
+
+    # This is to smooth the spectrum over a window of 10.
+    roll_win = np.ones(10) / 10
+    spec_seis = np.convolve(spec_seis, roll_win, mode='same')
+
+  return(freq_seis, spec_seis)      
+      
 def slicing(cube, type, loc, a_line):
 
   """
@@ -174,146 +309,6 @@ def display_slice(slices, type, b_line, c_line, cmap, vmin, vmax):
     extent = [b_line[0], b_line[-1], c_line[-1], c_line[0]]
     p1 = plt.imshow(slices.T, vmin=vmin, vmax=vmax, aspect='auto', extent=extent, cmap=cmap)
     plt.colorbar(p1)
-
-def frequency_spectrum(data, type, il_array, xl_array, twt_array, sample_rate):
-
-  """
-  Compute the frequency spectrum of the whole cube, inline, or crossline
-  cube
-
-  Input:
-
-  data: if you choose 'whole', data must be in 3D numpy array, if you choose
-        either 'il' or 'xl', data must be in 2D numpy array (you should make
-        the slice first using 'slicing' function)
-  type: 'whole' for the whole cube, 'il' for inline section, 'xl' for
-        crossline section 
-
-  il_array (inline), xl_array (xline), twt_array (time) depend on the type
-  you're choosing.
-
-  * If type = 'il', then il_array = None, specify your xl_array and twt_array
-  * If type = 'xl', then xl_array = None, specify your il_array and twt_array
-  * If type = 'whole', then specify all your il_array, xl_array, and twt_array
-
-  sample_rate: sampling rate of seismic data (in second) 
-
-  Output:
-
-  freq_seis: frequency
-  spec_seis: seismic amplitude
-  
-  """
-
-  import numpy as np
-  import matplotlib.pyplot as plt
-
-  if type == 'il':
-
-    # transpose the slice 
-    transp_slice = np.transpose(data)
-
-    # take the average of each individual line traces
-
-    # time
-    min_time = 0
-    max_time = len(twt_array)
-
-    # crosslines
-    xmin = 0
-    xmax = len(xl_array)
-
-    trace = np.mean(transp_slice[min_time:max_time, xmin:xmax], axis=1)
-
-    Fs_seis = 1 / sample_rate  # Seconds.
-    n_seis = len(trace)
-    k_seis = np.arange(n_seis)
-    T_seis = n_seis / Fs_seis
-    freq_seis = k_seis / T_seis
-    freq_seis = freq_seis[range(n_seis//2)]  # One side frequency range.
-
-    spec_seis = np.fft.fft(trace) / n_seis  # FFT computing and normalization.
-    spec_seis = spec_seis[range(n_seis//2)]
-
-    # This is to smooth the spectrum over a window of 10.
-    roll_win = np.ones(10) / 10
-    spec_seis = np.convolve(spec_seis, roll_win, mode='same')
-
-  if type == 'xl':
-
-    # transpose the slice 
-    transp_slice = np.transpose(data)
-
-    # take the average of each individual line traces
-
-    # time
-    min_time = 0
-    max_time = len(twt_array)
-
-    # crosslines
-    xmin = 0
-    xmax = len(il_array)
-
-    trace = np.mean(transp_slice[min_time:max_time, xmin:xmax], axis=1)
-
-    Fs_seis = 1 / sample_rate  # Seconds.
-    n_seis = len(trace)
-    k_seis = np.arange(n_seis)
-    T_seis = n_seis / Fs_seis
-    freq_seis = k_seis / T_seis
-    freq_seis = freq_seis[range(n_seis//2)]  # One side frequency range.
-
-    spec_seis = np.fft.fft(trace) / n_seis  # FFT computing and normalization.
-    spec_seis = spec_seis[range(n_seis//2)]
-
-    # This is to smooth the spectrum over a window of 10.
-    roll_win = np.ones(10) / 10
-    spec_seis = np.convolve(spec_seis, roll_win, mode='same')
-  
-  if type == 'whole':
-
-    # transpose the cube
-    transp_cube = np.transpose(data)
-
-    # time
-    min_time = 0
-    max_time = len(twt_array)
-
-    # crosslines
-    xmin = 0
-    xmax = len(xl_array)
-
-    # inlines
-    ymin = 0
-    ymax = len(il_array)
-
-    mean_xl_traces = [] # mean of crossline traces of each inline section
-
-    for i in range(ymax):
-      mean_xl = np.mean(transp_cube[min_time:max_time, xmin:xmax, i], axis=1)
-      mean_xl_traces.append(mean_xl)
-
-    transp_xl = np.transpose(mean_xl_traces)
-
-    # take average of each individual mean values of xl in the inline section
-
-    trace = np.mean(transp_xl[min_time:max_time, ymin:ymax], axis=1)
-
-    Fs_seis = 1 / sample_rate  # Seconds.
-    n_seis = len(trace)
-    k_seis = np.arange(n_seis)
-    T_seis = n_seis / Fs_seis
-    freq_seis = k_seis / T_seis
-    freq_seis = freq_seis[range(n_seis//2)]  # One side frequency range.
-
-    spec_seis = np.fft.fft(trace) / n_seis  # FFT computing and normalization.
-    spec_seis = spec_seis[range(n_seis//2)]
-
-    # This is to smooth the spectrum over a window of 10.
-    roll_win = np.ones(10) / 10
-    spec_seis = np.convolve(spec_seis, roll_win, mode='same')
-
-  return(freq_seis, spec_seis)
 
 def attribute_input(slices, type):
   """
